4ff3e5b31ad53455f03f86ba21a936c4f1e8412f
ahwangtingbiao <ahwangtingbiao@163.com>
2016-12-30 10:35:30 +0800

    skin-1.0

--- skins/js/common.js
+++ skins/js/common.js
@@ -6,18 +6,17 @@ $(document).ready(function () {
 		NProgress.done();
 	});
 
+	//banner打开微博
+	$(".banner-weibo").click(function () {
+		window.location.href = "http://m.weibo.cn/u/1136590322";
+	});
 
-
+	//打开皮肤详情
 	$(".skin-id").click(function () {
-		var skinID = $('.skin-id').attr('id');
+		var skinID = $(this).attr('id');
 		window.location.href = "item.html?id=" + skinID;
 
 	});
 
-	$(".banner-weibo").click(function () {
-
-		window.location.href = "http://m.weibo.cn/u/1136590322";
-
-	});
 
 });
