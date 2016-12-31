efc15f901afe074a651c0ddea3951630f4261cbc
unknown <ahwangtingbiao@163.com>
2017-01-01 03:14:04 +0800

    skins-item-1.0

--- skins/js/item.js
+++ skins/js/item.js
@@ -69,7 +69,7 @@ function skinInfo(name, author, size, description, update, star, type) {
     } else {
         //预览图切换	
         $(".preview").click(function () {
-                $(this).hide(100).addClass("none").siblings().removeClass("none").show(100);
+                $(this).addClass("none").siblings().removeClass("none");
             })
             //预览图
         $(".preview_9").attr("src", "res/it/" + skinID + "_9.jpg");
